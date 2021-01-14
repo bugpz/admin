c1b43f38cf6cc6735988c7c2e300c4a51dfb3314
bugpz <13558622779@163.com>
2021-01-14 15:55:08 +0800

    修改导航菜单
    Signed-off-by: bugpz <13558622779@163.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,12 +2,12 @@
   <el-row>
     <el-col :span="12" class="e_col">
       <el-menu
-        :collapse-transition="true"
+        :collapse-transition="false"
         :default-active="$route.path"
         class="el-menu-vertical-demo"
         @open="handleOpen"
         @close="handleClose"
-        background-color="#758fa9"
+        background-color="#304156"
         text-color="#fff"
         router
       >
@@ -17,19 +17,30 @@
         </el-menu-item>
         <el-submenu index="1">
           <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>导航一</span>
+            <i class="el-icon-data-line"></i>
+            <span>营销管理</span>
           </template>
-          <el-menu-item-group>
-            <el-menu-item index="/h">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-<!--          <el-menu-item-group title="分组2">-->
-            <el-menu-item index="1-3">选项3</el-menu-item>
-<!--          </el-menu-item-group>-->
+          <el-submenu index="1-1">
+            <template slot="title">优惠券管理</template>
+            <el-menu-item index="/h">平台优惠券管理</el-menu-item>
+            <el-menu-item index="/1-1-1">平台优惠券审核</el-menu-item>
+            <el-menu-item index="/1-1-2">商家优惠券管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="1-2">
+            <template slot="title">购物送券</template>
+            <el-menu-item index="1-2-1">购物送券申请</el-menu-item>
+            <el-menu-item index="1-2-2">购物送券审核</el-menu-item>
+          </el-submenu>
+          <el-submenu index="1-3">
+            <template slot="title">人工发券</template>
+            <el-menu-item index="1-3-1">人工发券申请</el-menu-item>
+            <el-menu-item index="1-3-2">人工发券审核</el-menu-item>
+          </el-submenu>
           <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
+            <template slot="title">营销场景</template>
+            <el-menu-item index="1-4-1">邀请有礼设置</el-menu-item>
+            <el-menu-item index="1-4-2">领券中心统计</el-menu-item>
+            <el-menu-item index="1-4-3">新人礼统计</el-menu-item>
           </el-submenu>
         </el-submenu>
         <el-menu-item index="2">
