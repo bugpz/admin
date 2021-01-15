548dd90c9b16b9a9dcf285b754ee4007d625fe6d
bugpz <13558622779@163.com>
2021-01-15 23:39:24 +0800

    add: page-repertory

---
+++ src/page/repertory/repertoryAlter.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'repertoryAlter'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/page/repertory/repertoryDistribution.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'repertoryDistribution'
+}
+</script>
+
+<style scoped>
+
+</style>
