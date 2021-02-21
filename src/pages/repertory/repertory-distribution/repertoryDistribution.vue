a7c89f6ab39dfac0a1fe5cc694317eb1518b7655
bugpz <13558622779@163.com>
2021-02-22 00:15:58 +0800

    fix: 商品库存查询页面查询功能及数据渲染完成

--- src/pages/repertory/repertory-distribution/repertoryDistribution.vue
+++ src/pages/repertory/repertory-distribution/repertoryDistribution.vue
@@ -10,16 +10,12 @@
           >
             <el-form-item
               label="基础商品编码"
-              :rules="[
-          {required: true, message: '请输入邮箱地址', trigger: 'blur'}
-        ]"
             >
               <el-input
-                placeholder="基础商品编码"
-                v-model="input"
+                v-model="inputCode"
                 size="medium"
-                style="width: 100%"
-                maxlength=10
+                maxlength=12
+                :placeholder="inputMsg"
               >
               </el-input>
             </el-form-item>
@@ -28,34 +24,36 @@
             :span=16
           >
             <el-form-item class="right_btn">
-              <el-button type="primary" size="small">查询</el-button>
+              <el-button type="primary" size="small" @click="handleSelect">查询</el-button>
               <el-button type="success" size="small" native-type="reset">清空</el-button>
             </el-form-item>
           </el-col>
         </el-row>
       </el-form>
     </div>
-    <div class="form">
+    <div>
       <el-table
-        border
-        style="width: 100%"> <!--:data="tableData"-->
+        :data="tableLists"
+      >
         <el-table-column
-          prop="date"
           label="规格"
-          >
+        >
+          <template
+            slot-scope="scope"
+          >{{JSON.parse(scope.row.skuInfo.substring(1, scope.row.skuInfo.length - 1)).name}}</template>
         </el-table-column>
         <el-table-column
-          prop="name"
+          prop="saleStock"
           label="销售库存数"
         >
         </el-table-column>
         <el-table-column
-          prop="address"
+          prop="virtualStock"
           label="虚拟库存数"
           >
         </el-table-column>
         <el-table-column
-        prop="can"
+        prop="availableStock"
         label="可用库存数"
         >
         </el-table-column>
@@ -65,11 +63,34 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'index',
   data () {
     return {
-      input: ''
+      inputCode: '',
+      inputMsg: '请输入12位数字的基础商品编码',
+      tableLists: []
+    }
+  },
+  methods: {
+    handleSelect () {
+      const url = '/api/web/commodity/stock/stockList'
+      if (this.inputCode.length === 12) {
+        axios
+          .post(url, {
+            'orderby': 'commodityStockType',
+            'sort': 'desc',
+            'searchs': '[{"tempMatchType":"4","propertyName":"commodityNo","propertyValue1":' + '"' + this.inputCode + '"' + ',"tempType":"String"}]'}, {
+            headers: {
+              authorization: localStorage.getItem('token')
+            }})
+          .then(res => {
+            this.tableLists = res.data.result
+          })
+      } else {
+        this.inputCode = ''
+      }
     }
   }
 }
