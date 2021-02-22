d63cea87cf530ec26e4b59165e4ea300e2beccf8
bugpz <13558622779@163.com>
2021-02-22 19:41:42 +0800

    fix：完成基本的搜索功能

--- src/pages/repertory/repertoryAlter/repertoryAlter.vue
+++ src/pages/repertory/repertoryAlter/repertoryAlter.vue
@@ -5,7 +5,7 @@
  * @LastEditTime: 2021-01-24 17:18
  * @LastEditors: bug_pz
 -->
-<template xmlns:el-col="http://www.w3.org/1999/html">
+<template>
   <div>
     <div class="form">
       <el-form
@@ -23,7 +23,7 @@
             >
               <el-input
                 placeholder="商品基础编码"
-                v-model="input"
+                v-model="inputCode"
                 manlength=10
                 size="medium"
               >
@@ -71,34 +71,80 @@
         </el-row>
         <el-row>
           <el-button class="right_btn" type="success">清空</el-button>
-          <el-button class="right_btn" type="primary">查询</el-button>
+          <el-button class="right_btn" type="primary" @click="handleSelect">查询</el-button>
         </el-row>
       </el-form>
     </div>
-    <div class="form">
-      <el-table
-        style="width: 100%"
-      >
-        <el-table-column label="商品编码"></el-table-column>
-        <el-table-column label="库存类型"></el-table-column>
-        <el-table-column label="变动类型"></el-table-column>
-        <el-table-column label="库存变动"></el-table-column>
-        <el-table-column label="变动后库存"></el-table-column>
-        <el-table-column label="变动时间"></el-table-column>
-        <el-table-column label="标识"></el-table-column>
-        <el-table-column label="变动说明"></el-table-column>
-        <el-table-column label="变动来源"></el-table-column>
-      </el-table>
+    <div>
+      <el-row>
+        <el-col
+          :span="4"
+          style="height: 5vh; text-align: center"
+        >
+          <span
+            :v-model="skuList.skuNo"
+            @click="handleSelectSku"
+          >
+            <el-button>{{ skuList.specificationValue.name }}</el-button>
+          </span>
+        </el-col>
+        <el-col
+          :span="20"
+        >
+          <el-table
+            :data="tableLists"
+          >
+            <el-table-column
+              label="商品编码"
+              prop="commodityNo"
+            ></el-table-column>
+            <el-table-column
+              label="库存类型"
+              prop="commodityStockTypeDesc"
+            ></el-table-column>
+            <el-table-column
+              label="变动类型"
+              prop="stockChangeTypeDesc"
+            ></el-table-column>
+            <el-table-column
+              label="库存变动"
+              prop="stockChange"
+            ></el-table-column>
+            <el-table-column
+              label="变动后库存"
+              prop="stock"
+            ></el-table-column>
+            <el-table-column
+              label="变动时间"
+              prop="changeTime"
+            ></el-table-column>
+            <el-table-column
+              label="标识"
+              prop="changeSigns"
+            ></el-table-column>
+            <el-table-column
+              label="变动说明"
+              prop="stockChangeDescribeDesc"
+            ></el-table-column>
+            <el-table-column
+              label="变动来源"
+              prop="stockChangeSourceDesc"
+            ></el-table-column>
+          </el-table>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import {LoginStatusVerification} from '../../../utils/selectLoginStatus'
 export default {
   name: 'repertoryAlter',
   data () {
     return {
-      input: '',
+      inputCode: '',
       value: '',
       valueT: '',
       options: [
@@ -111,7 +157,72 @@ export default {
         {value: '选项二', label: '订单'},
         {value: '选项三', label: '商品'},
         {value: '选项四', label: 'OMS'}
-      ]
+      ],
+      skuList: {'specificationValue': 'sku'},
+      tableLists: []
+    }
+  },
+  methods: {
+    handleSelect () {
+      this.skuList = {'specificationValue': 'sku'} // 每次搜索时初始化,不然新请求没值时会显示上一次返回的值
+      const url = '/api/web/commodity/stock/querySkuInfoByCommodity'
+      const tableUrl = '/api/web/commodity/stock/stockChangeLogList'
+      axios
+        .post(url,
+          {
+            'commodityNo': this.inputCode
+          }, {
+            headers: {
+              authorization: localStorage.getItem('token')
+            }})
+        .then(res => {
+          if (res.data.code === 200) {
+            const resultSku = res.data.result
+            for (let i in resultSku) {
+              console.log(resultSku[i].specificationValue)
+              const specificationValue = JSON.parse(resultSku[i].specificationValue.substring(1, resultSku[i].specificationValue.length - 1)) // JSON.parse(resultSku[i].specificationValue.substring(1, resultSku[i].specificationValue.length - 1))
+              // this.skuList = JSON.parse(resultSku[i].specificationValue.substring(1, resultSku[i].specificationValue.length - 1))
+              this.skuList = {'skuNo': resultSku[i].skuNo, 'specificationValue': specificationValue}
+              console.log(specificationValue)
+              console.log(this.skuList)
+            }
+            axios
+              .post(tableUrl, {
+                'pageNo': 1,
+                'pageSize': 20,
+                'orderby': 'createTime',
+                'sort': 'desc',
+                'searchs': '[{"tempMatchType":"4","propertyName":"commodityNo","propertyValue1":' + '"' + this.inputCode + '"' + ',"tempType":"String"},' +
+                  '{"tempMatchType":"4","propertyName":"skuNo","propertyValue1":' + this.skuList.skuNo + ',"tempType":"Integer"}]'}, {
+                headers: {
+                  authorization: localStorage.getItem('token')
+                }
+              })
+              .then(res => {
+                this.tableLists = res.data.result.itemVOs
+              })
+          } else if (res.data.code === 401) {
+            LoginStatusVerification()
+          }
+        })
+    },
+    handleSelectSku () { // 点击sku时的搜索
+      const tableUrl = '/api/web/commodity/stock/stockChangeLogList'
+      axios
+        .post(tableUrl, {
+          'pageNo': 1,
+          'pageSize': 20,
+          'orderby': 'createTime',
+          'sort': 'desc',
+          'searchs': '[{"tempMatchType":"4","propertyName":"commodityNo","propertyValue1":' + '"' + this.inputCode + '"' + ',"tempType":"String"},' +
+            '{"tempMatchType":"4","propertyName":"skuNo","propertyValue1":' + this.skuList.skuNo + ',"tempType":"Integer"}]'}, {
+          headers: {
+            authorization: localStorage.getItem('token')
+          }
+        })
+        .then(res => {
+          this.tableLists = res.data.result.itemVOs
+        })
     }
   }
 }
